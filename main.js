66f8ec3b055c2c87ae713922137062737c640926
Adarsh Kumar <86519736+AdarshKumarDash@users.noreply.github.com>
2022-03-02 19:30:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 //https://teachablemachine.withgoogle.com/models/rAhAqBC-v/
+
+function record() {
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/rAhAqBC-v/model.json", modelready);
+}
+
+function modelready() {
+    console.log("Model Is Ready");
+    classifier.classify(gotResult);
+}
